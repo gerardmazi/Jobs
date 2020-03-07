a9e41d5c16ab576ec0c61334ab13632f33299dfd
Gerard Mazi <gerard.mazi@gmail.com>
2020-03-07 10:23:18 -0800

    Trial
    Initial trial

--- Job_Scraper.py
+++ Job_Scraper.py
@@ -16,5 +16,12 @@ from datetime import date
 
 driver = webdriver.Chrome(r"chromedriver.exe")
 
-link = 'https://www.linkedin.com/jobs/search/?f_C=37759&locationId=OTHERS.worldwide'
+link = 'https://www.linkedin.com/jobs/search/?f_C=17719&locationId=OTHERS.worldwide'
 
+driver.get(link)
+driver.find_element_by_xpath('//li[@class = "occludable-update artdeco-list__item--offset-4 artdeco-list__item p0 ember-view"]').click()
+driver.find_element_by_xpath('//h2[@class = "jobs-details-top-card__job-title t-20 t-black t-normal"]').text
+driver.find_element_by_xpath('//a[@class = "jobs-details-top-card__company-url ember-view"]').text
+driver.find_element_by_xpath('//a[@class = "jobs-details-top-card__exact-location t-black--light link-without-visited-state"]').text
+driver.find_element_by_xpath('//ul[@class = "jobs-box__list jobs-description-details__list js-formatted-industries-list"]').text
+driver.find_element_by_xpath('//div[@class = "jobs-box__html-content jobs-description-content__text t-14 t-black--light t-normal"]').text
